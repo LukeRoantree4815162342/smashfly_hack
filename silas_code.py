48575e02988e03649f2ea7386d18238affd7830a
Luke Roantree <lroantree@144145-ctamop-lroantree.local>
2019-10-26 17:55:21 +0100

    sending file

--- silas_code.py
+++ silas_code.py
@@ -1,12 +1,39 @@
-#Load dataset and turn into pandas dataframe
+job_dfs = [successful_df[successful_df[j]==True] for j in jobs]
 
-Smashfly = pd.read_csv("SF_Datathon_Data.csv", encoding='utf-8')
-sf_df = pd.DataFrame(Smashfly)
-print(sf_df.head())
-jobs_unique = sf_df['Job_Type'].unique()
+age_stats = {jobs_unique[i]: {"kurtosis":df["Age"].kurtosis(), "skew":df["Age"].skew()} for i,df in enumerate(job_dfs)}
 
-cat_columns = ["Qualification_type", "Job_Type", "Race"]
-print(sf_df.columns)
-sf_df = pd.get_dummies(sf_df, prefix_sep="_", columns=cat_columns)
+#print(age_stats)
+#plt.figure()
 
-print(sf_df.columns)
+statistics = ["skew", "kurtosis"]
+fig, ax = plt.subplots(len(statistics), sharex=True)
+
+for row in range(len(statistics)):
+    ax[row].bar(range(len(jobs)), [age_stats[j][statistics[row]] for j in jobs_unique])
+    ax[row].set_xticks(range(len(jobs)))
+    ax[row].set_xticklabels([j.replace(' ','\n') for j in jobs_unique], rotation=45, size=9)
+    ax[row].set_ylabel(statistics[row])
+fig.suptitle("Description of Interviewed Applicant Age by Job Type", size=10)
+
+"""
+and;
+"""
+
+job_dfs = [successful_df[successful_df[j]==True] for j in jobs]
+
+age_stats = {jobs_unique[i]: df["Age"].describe() for i,df in enumerate(job_dfs)}
+
+#print(age_stats)
+#plt.figure()
+
+statistics = ["mean", "std", "50%", "25%", "75%"]
+fig, ax = plt.subplots(len(statistics), sharex=True)
+
+for row in range(len(statistics)):
+    ax[row].bar(range(len(jobs)), [age_stats[j][statistics[row]] for j in jobs_unique])
+    ax[row].set_xticks(range(len(jobs)))
+    ax[row].set_xticklabels([j.replace(' ','\n') for j in jobs_unique], rotation=45, size=9)
+    ax[row].set_ylabel(statistics[row])
+fig.suptitle("Description of Interviewed Applicant Age by Job Type", size=10)
+
+
