12409d4ab4e3913f07707fb22352ddd65e12eebf
Luke Roantree <lroantree@144145-ctamop-lroantree.local>
2019-10-27 00:39:11 +0100

    data splitting

--- silas_code.py
+++ silas_code.py
@@ -1,18 +1,29 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
-Smashfly = pd.read_csv("SF_Datathon_Data.csv", encoding='utf-8')
-sf_df = pd.DataFrame(Smashfly)
-#print(sf_df.head())
-jobs_unique = sf_df['Job_Type'].unique()
 
-cat_columns = ["Qualification_type", "Job_Type", "Race"]
-#print(sf_df.columns)
-sf_df = pd.get_dummies(sf_df, prefix_sep="_", columns=cat_columns)
-sf_df["Gender_M"] = sf_df["Gender"] == "M"
-sf_df["Gender_F"] = sf_df["Gender"] == "F"
-sf_df["Gender_Unspecified"] = 1 - sf_df["Gender_F"] - sf_df["Gender_M"]
-sf_df["Gender_Unspecified"] = sf_df["Gender_Unspecified"].astype('bool')
-del sf_df['Gender']
-del sf_df['Name']
-sf_df.loc[sf_df['Qualification_type_1']+sf_df['Qualification_type_2']+sf_df['Qualification_type_3'] == 0, ['GPA']] = np.NaN
+train, test = train_test_split(sf_df, test_size=0.15)
 
-print(sf_df.columns)
+features = ['id', 'Age', 'Qualification_type_1',
+       'Qualification_type_2', 'Qualification_type_3', 'Qualification_type_4',
+       'Qualification_type_5', 'Job_Type_Accounting',
+       'Job_Type_Business Development', 'Job_Type_Engineering',
+       'Job_Type_Human Resources', 'Job_Type_Legal', 'Job_Type_Marketing',
+       'Job_Type_Product Management', 'Job_Type_Research and Development',
+       'Job_Type_Sales', 'Job_Type_Services', 'Job_Type_Support',
+       'Job_Type_Training', 'Race_Asian', 'Race_Black', 'Race_Latino',
+       'Race_Other', 'Race_White', 'Gender_M', 'Gender_F',
+       'Gender_Unspecified']
+
+X = train[features]
+Y = train['Interviewed']
+
+model1 = DecisionTreeClassifier(max_depth=4, min_samples_leaf=3)
+model2 = AdaBoostClassifier(learning_rate=1, n_estimators=200, base_estimator=model1)
+
+#model1.fit(X,Y)
+for epoch in range(20):
+    model2.fit(X,Y)
+
+model2.score(X,Y)
